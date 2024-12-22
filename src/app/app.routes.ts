b8d20f66219ddf97d5a54b95086506c17b679f0f
Alan <alanoutmartins@gmail.com>
2024-12-22 11:02:35 -0300

    alterações na aba de compras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { PessoaComponent } from './components/pessoa/pessoa.component';
 import { provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CompraComponent } from './components/compra/compra.component';
 
 export const routes: Routes = [
-    {path: 'pessoas', component: PessoaComponent}
+    {path: 'pessoas', component: PessoaComponent},
+    {path: 'compras', component: CompraComponent}
 ];
 
 bootstrapApplication(AppComponent, {
